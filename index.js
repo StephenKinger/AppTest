d8e2c6b1a756a1772939f67777e9c0d805399155
Stephen Kinger <stephen.kinger@mappy.com>
2021-12-23 12:58:06 +0100

    End

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const sqlite3 = require("sqlite3").verbose();
+const paginate = require('express-paginate');
 
 // Création du serveur Express
 const app = express();
@@ -10,9 +11,11 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: false }));
+app.use(paginate.middleware(10, 50));
+
 
 // Connexion à la base de donnée SQlite
-const db_name = path.join(__dirname, "data", "apptest.db");
+const db_name = path.join(__dirname, "data", "db.sqlite");
 const db = new sqlite3.Database(db_name, err => {
   if (err) {
     return console.error(err.message);
@@ -21,28 +24,12 @@ const db = new sqlite3.Database(db_name, err => {
 });
 
 // Création de la table Livres (Livre_ID, Titre, Auteur, Commentaires)
-const sql_create = `CREATE TABLE IF NOT EXISTS Livres (
-  Livre_ID INTEGER PRIMARY KEY AUTOINCREMENT,
-  Titre VARCHAR(100) NOT NULL,
-  Auteur VARCHAR(100) NOT NULL,
-  Commentaires TEXT
-);`;
-db.run(sql_create, err => {
+const sql_show = `SELECT * FROM 'users' LIMIT 0,30;`;
+db.run(sql_show, err => {
   if (err) {
     return console.error(err.message);
   }
-  console.log("Création réussie de la table 'Livres'");
-  // Alimentation de la table
-  const sql_insert = `INSERT INTO Livres (Livre_ID, Titre, Auteur, Commentaires) VALUES
-  (1, 'Mrs. Bridge', 'Evan S. Connell', 'Premier de la série'),
-  (2, 'Mr. Bridge', 'Evan S. Connell', 'Second de la série'),
-  (3, 'L''ingénue libertine', 'Colette', 'Minne + Les égarements de Minne');`;
-  db.run(sql_insert, err => {
-    if (err) {
-      return console.error(err.message);
-    }
-    console.log("Alimentation réussie de la table 'Livres'");
-  });
+  console.log("Création réussie de la table 'users'");
 });
 
 // Démarrage du serveur
@@ -56,93 +43,98 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-// GET /about
-app.get("/about", (req, res) => {
-  res.render("about");
-});
 
 // GET /data
-app.get("/data", (req, res) => {
-  const test = {
-    titre: "Test",
-    items: ["un", "deux", "trois"]
-  };
-  res.render("data", { model: test });
+app.get("/user/:id", (req, res) => {
+  let query="SELECT * FROM users WHERE id = "+req.params.id;
+  db.get(query, [], (err, result) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    query="SELECT url FROM 'media' WHERE userId = "+req.params.id;
+    db.get(query, [], (err, media) => {
+      if (err) {
+        return console.error(err.message);
+      }
+      query="SELECT * FROM messages WHERE senderId = "+req.params.id +" OR receiverId = "+req.params.id+";";
+      console.log(query);
+      db.all(query, [], (err, messages) => {
+        if (err) {
+          return console.error(err.message);
+        }
+        //TODO complete with names of emitters and receivers
+        res.render("user", { user: result,
+                            messages: messages,
+                            media: media});
+      });
+    });
+  });
+
 });
 
-// GET /livres
-app.get("/livres", (req, res) => {
-  const sql = "SELECT * FROM Livres ORDER BY Titre";
+
+
+function generateSQLQuery(isdeleted, username, country, name, city, limit, skip) {
+  let queryfilter = "";
+  console.log(isdeleted + username + country +  name + city);
+  if (isdeleted != "" && isdeleted != undefined){
+    //TODO Use transformation to not set null or 1 in the front end to filter on this
+
+    if (isdeleted == 'true') {
+      queryfilter += "AND isDeleted = true";
+    }
+
+    else {
+      queryfilter += "AND isDeleted is NULL";
+    }
+  }
+  if (username){
+    queryfilter += "AND username LIKE '%"+username+"%'";
+  }
+  if (country){
+    queryfilter += "AND country LIKE '%"+country+"%'";
+  }
+  if (name){
+    queryfilter += "AND name LIKE '%"+name+"%'";
+  }
+  if (city){
+    queryfilter += "AND city LIKE '%"+city+"%'";
+  }
+  //Remove the first AND
+  queryfilter = queryfilter.substring(4);
+  console.log(queryfilter);
+  let query = "SELECT * FROM 'users'";
+  if (queryfilter != "") {
+    query += " WHERE "+queryfilter;
+  }
+  if (skip === undefined){
+    skip = 0;
+  }
+  query += " ORDER BY id LIMIT "+limit+" OFFSET "+skip;
+  console.log(query);
+  return query;
+}
+
+app.get("/users", async (req, res, next) => {
+  console.log(req.query.page)
+  const sql = generateSQLQuery(req.query.isdeleted,
+                              req.query.username,
+                            req.query.country,
+                          req.query.name,
+                        req.query.city,
+                      req.query.limit,
+                    req.query.limit*(req.query.page-1))
+
+  // TODO : set request async
+  const result = await db.get("SELECT count(id) FROM 'users'", []);
+  const pageCount = Math.ceil(result.row / req.query.limit);
   db.all(sql, [], (err, rows) => {
     if (err) {
       return console.error(err.message);
     }
-    res.render("livres", { model: rows });
-  });
-});
-
-// GET /create
-app.get("/create", (req, res) => {
-  res.render("create", { model: {} });
-});
-
-// POST /create
-app.post("/create", (req, res) => {
-  const sql = "INSERT INTO Livres (Titre, Auteur, Commentaires) VALUES (?, ?, ?)";
-  const book = [req.body.Titre, req.body.Auteur, req.body.Commentaires];
-  db.run(sql, book, err => {
-    if (err) {
-      return console.error(err.message);
-    }
-    res.redirect("/livres");
-  });
-});
-
-// GET /edit/5
-app.get("/edit/:id", (req, res) => {
-  const id = req.params.id;
-  const sql = "SELECT * FROM Livres WHERE Livre_ID = ?";
-  db.get(sql, id, (err, row) => {
-    if (err) {
-      return console.error(err.message);
-    }
-    res.render("edit", { model: row });
-  });
-});
-
-// POST /edit/5
-app.post("/edit/:id", (req, res) => {
-  const id = req.params.id;
-  const book = [req.body.Titre, req.body.Auteur, req.body.Commentaires, id];
-  const sql = "UPDATE Livres SET Titre = ?, Auteur = ?, Commentaires = ? WHERE (Livre_ID = ?)";
-  db.run(sql, book, err => {
-    if (err) {
-      return console.error(err.message);
-    }
-    res.redirect("/livres");
-  });
-});
-
-// GET /delete/5
-app.get("/delete/:id", (req, res) => {
-  const id = req.params.id;
-  const sql = "SELECT * FROM Livres WHERE Livre_ID = ?";
-  db.get(sql, id, (err, row) => {
-    if (err) {
-      return console.error(err.message);
-    }
-    res.render("delete", { model: row });
-  });
-});
-
-// POST /delete/5
-app.post("/delete/:id", (req, res) => {
-  const id = req.params.id;
-  const sql = "DELETE FROM Livres WHERE Livre_ID = ?";
-  db.run(sql, id, err => {
-    if (err) {
-      return console.error(err.message);
-    }
-    res.redirect("/livres");
+    res.render("users", { model: rows,
+                          count: pageCount,
+                          currentPage: req.query.page,
+                        });
   });
 });
